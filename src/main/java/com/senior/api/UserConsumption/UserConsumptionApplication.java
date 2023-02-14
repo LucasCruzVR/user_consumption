a08a5685fd3ddb3f76220694658bc1eef92ce2e6
lucas cruz <lucas_cruz100@live.com>
2023-02-14 20:26:36 -0300

    project created

---
+++ src/main/java/com/senior/api/UserConsumption/UserConsumptionApplication.java
@@ -0,0 +1,13 @@
+package com.senior.api.UserConsumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserConsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserConsumptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senior/api/UserConsumption/UserConsumptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.api.UserConsumption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserConsumptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
