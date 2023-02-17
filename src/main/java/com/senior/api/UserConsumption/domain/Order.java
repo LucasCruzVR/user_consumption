946f32f7a687e0e9054a326b3428a01bd368a930
lucas cruz <lucas_cruz100@live.com>
2023-02-17 19:10:46 -0300

    validation for discount

--- src/main/java/com/senior/api/UserConsumption/domain/Order.java
+++ src/main/java/com/senior/api/UserConsumption/domain/Order.java
@@ -4,6 +4,8 @@ import com.senior.api.UserConsumption.itemize.OrderStatusEnum;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -27,6 +29,8 @@ public class Order implements Serializable {
     @NotEmpty(message = "Order code can't be empty")
     private String orderCode;
 
+    @Min(value = 0, message = "Discount needs to be between 0 and 100")
+    @Max(value = 100, message = "Discount needs to be between 0 and 100")
     private Double discountPercentage;
 
     @Enumerated(EnumType.STRING)
--- src/main/java/com/senior/api/UserConsumption/model/order/OrderUpdateDTO.java
+++ src/main/java/com/senior/api/UserConsumption/model/order/OrderUpdateDTO.java
@@ -4,10 +4,14 @@ import com.senior.api.UserConsumption.itemize.OrderStatusEnum;
 import com.senior.api.UserConsumption.model.order_item.OrderItemCreateDTO;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Set;
 
 @Data
 public class OrderUpdateDTO {
+    @Min(value = 0, message = "Discount needs to be between 0 and 100")
+    @Max(value = 100, message = "Discount needs to be between 0 and 100")
     private Double discountPercentage;
     private OrderStatusEnum status;
     private Set<OrderItemCreateDTO> orderItem;
